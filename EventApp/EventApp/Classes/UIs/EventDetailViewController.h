0f32de85b36a6e915de80d8b834fb87b28f657f7
Nong Trung Nghia <nghianong.bhtech@gmail.com>
2014-11-17 10:36:00 +0700

    init

---
+++ EventApp/EventApp/Classes/UIs/EventDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  EventDetailViewController.h
+//  EventApp
+//
+//  Created by Tommy on 11/17/14.
+//  Copyright (c) 2014 BHTech Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventDetailViewController : UIViewController
+
+@end
---
+++ EventApp/EventApp/Classes/UIs/EventListViewController.h
@@ -0,0 +1,13 @@
+//
+//  EventListViewController.h
+//  EventApp
+//
+//  Created by Tommy on 11/17/14.
+//  Copyright (c) 2014 BHTech Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventListViewController : UIViewController
+
+@end
