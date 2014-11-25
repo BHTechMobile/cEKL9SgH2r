950d272122f80d76c3269950a1cb75f8dc11ed1f
Nong Trung Nghia <nghianong.bhtech@gmail.com>
2014-11-25 13:49:19 +0700

    update services

--- EventApp/EventApp/Classes/Utilities/JSONLoader.h
+++ EventApp/EventApp/Classes/Utilities/JSONLoader.h
@@ -5,18 +5,14 @@
 //
 
 #import <Foundation/Foundation.h>
-#import <AFNetworking/AFNetworking.h>
+#import <AFNetworking.h>
 
 typedef void (^SuccessBlock)(AFHTTPRequestOperation *operation, id responseObject);
 typedef void (^FailureBlock)(AFHTTPRequestOperation *operation, NSError *error);
-typedef void (^ProgressBlock)(NSUInteger bytesWritten, long long totalBytesWritten, long long totalBytesExpectedToWrite);
-
-typedef void (^MessageBlock)(NSString* bodyString, NSError *error);
-typedef void (^DownloadResponseBlock)(float progress);
 
 @interface JSONLoader : NSObject
-+(AFHTTPRequestOperationManager*)sharedManager;
 
-+(void)getJsonListWithURL:(NSURL *)url success:(void (^)( id response))success failure:(void (^)(NSError *error))failure;
++(AFHTTPRequestOperationManager*)sharedManager;
++(void)requestDataSuccess:(SuccessBlock)success failure:(FailureBlock)failure;
 
 @end
